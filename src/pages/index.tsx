294c209880b86ffe21fba08a0efd934e7b3afb40
pawelpiwowarski <pawelpiwowarski2000@gmail.com>
2023-05-14 23:59:56 +0200

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,258 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+export default function ConvolutionCalculator() {
+  const [inputWidth, setInputWidth] = useState(0);
+  const [inputHeight, setInputHeight] = useState(0);
+  const [inputChannels, setInputChannels] = useState(0);
+  const [kernelSize, setKernelSize] = useState(0);
+  const [padding, setPadding] = useState(0);
+  const [stride, setStride] = useState(0);
+  const [outputChannels, setOutputChannels] = useState(0);
+  const [outputWidth, setOutputWidth] = useState(0);
+  const [outputHeight, setOutputHeight] = useState(0);
 
-export default function Home() {
+
+  const calculateOutputSize = () => {
+    const outputWidth = Math.floor(
+      (inputWidth + 2 * padding - kernelSize) / stride + 1
+    );
+    const outputHeight = Math.floor(
+      (inputHeight + 2 * padding - kernelSize) / stride + 1
+    );
+
+    setOutputHeight(outputHeight);
+    setOutputWidth(outputWidth);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center space-y-4">
+    <div className="grid grid-cols-3 gap-4 pt-20">
+      <h1 className="col-span-3 text-3xl font-bold text-center pb-4">
+        Convolution Calculator
+      </h1>
+
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex items-center space-x-4">
+          <label htmlFor="inputWidth">Input Width:</label>
+          <input 
+          className="range range-accent"
+            type="range"
+            min="0"
+            max="100"
+            value={inputWidth}
+            onChange={(e) => setInputWidth(parseInt(e.target.value))}
+            id="inputWidth"
+          />
+          <input
+
+className="input"
+            type="number"
+            min="0"
+            max="100"
+            value={inputWidth}
+            onChange={(e) => setInputWidth(parseInt(e.target.value))}
+            onBlur={(e) => setInputWidth(parseInt(e.target.value))}
+          />  
+
+
         </div>
-      </div>
+        <div className="flex items-center space-x-4">
+          <label htmlFor="inputHeight">Input Height:</label>
+          <input
+            className="range range-accent"
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+            type="range"
+            min="0"
+            max="100"
+            value={inputHeight}
+            onChange={(e) => setInputHeight(parseInt(e.target.value))}
+            id="inputHeight"
+          />
+  <input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={inputHeight  }
+    onChange={(e) => setInputHeight(parseInt(e.target.value))}
+    onBlur={(e) => setInputHeight(parseInt(e.target.value))}
+  />
+        </div>
+        <div className="flex items-center space-x-4">
+          <label htmlFor="inputChannels">Input Channels:</label>
+          <input
+            className="range range-accent"
+            type="range"
+            min="0"
+            max="100"
+            value={inputChannels}
+            onChange={(e) => setInputChannels(parseInt(e.target.value))}
+            id="inputChannels"
+          />
+  <input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={inputChannels  }
+    onChange={(e) => setInputChannels(parseInt(e.target.value))}
+    onBlur={(e) => setInputChannels(parseInt(e.target.value))}
+  />
+        </div>
+       
+  
+  </div>
+  <div className="flex flex-col items-center space-y-4">
+  <div className="flex items-center space-x-4">
+  <label htmlFor="kernelSize">Kernel Size:</label>
+  <input 
+    className="range range-accent"
+    type="range"
+    min="0"
+    max="100"
+    value={kernelSize}
+    onChange={(e) => setKernelSize(parseInt(e.target.value))}
+    id="kernelSize"
+  />
+  <input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={kernelSize}
+    onChange={(e) => setKernelSize(parseInt(e.target.value))}
+    onBlur={(e) => setKernelSize(parseInt(e.target.value))}
+  />
+</div>
+
+        <div className="flex items-center space-x-4 pt-4">
+          <label htmlFor="inputHeight">Padding:</label>
+          <input
+            className="range range-accent"
+
+            type="range"
+            min="0"
+            max="100"
+            value={padding}
+            onChange={(e) => setPadding(parseInt(e.target.value))}
+            id="padding"
+          />
+
+<input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={padding}
+    onChange={(e) => setPadding(parseInt(e.target.value))}
+    onBlur={(e) => setPadding(parseInt(e.target.value))}
+  />
+
+        </div>
+        <div className="flex items-center space-x-4 pt-6" >
+          <label htmlFor="inputChannels">Stride:</label>
+          <input
+            className="range range-accent"
+            type="range"
+            min="0"
+            max="100"
+            value={stride}
+            onChange={(e) => setStride(parseInt(e.target.value))}
+            id="stride"
+          />
+   <input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={stride}
+    onChange={(e) => setStride(parseInt(e.target.value))}
+    onBlur={(e) => setStride(parseInt(e.target.value))}
+  />
+        </div>
+       
+  
+  </div>
+
+
+  <div className="mt-4">
+
+
+      <div className="flex items-center space-x-4">
+        <label htmlFor="outputChannels">Output Channels:</label>
+        <input 
+className="range range-accent"
+          type="range"
+          min="0"
+          max="100"
+          value={outputChannels}
+          onChange={(e) => setOutputChannels(parseInt(e.target.value))}
+          id="outputChannels"
         />
+
+<input 
+    className="input"
+    type="number"
+    min="0"
+    max="100"
+    value={outputChannels}
+    onChange={(e) => setOutputChannels(parseInt(e.target.value))}
+    onBlur={(e) => setOutputChannels(parseInt(e.target.value))}
+  />
+
+      </div>
+<div className="flex items-center space-x-4 p-4 ml-6">
+
+    <button onClick={() => console.log(calculateOutputSize())} className="btn btn-outline btn-info">      Calculate Output Size</button>
+</div>
+    </div>
+
+
+  </div>
+
+  <div className="flex flex-col items-center space-y-4">
+
+</div>
+
+{
+ outputWidth > 0 && (
+    <div className="flex flex-col items-center space-y-4">
+      <h1 className="text-2xl font-bold text-center pb-4">
+        Output Size
+      </h1>
+      <div className="flex items-center space-x-4">
+        <p> {outputWidth} x {outputWidth} x {outputChannels} </p>
+
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
+
+}
+
+
+<footer className="flex items-center justify-center w-full h-24 border-t">
+
+<p className="text-center">
+  Built by {'Pawel Piwowarski'} 
+  </p>
+  <a className="ml-2" href="https://github.com/pawelpiwowarski"
+  target="_blank"
+  rel="noopener noreferrer"
+  >
+    <p className="text-center underline">
+
+Github
+    </p>
+  </a>
+
+
+
+
+
+</footer>
+
+</div>
+  );
+    
 }
